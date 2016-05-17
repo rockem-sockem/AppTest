5b532b59f92c1357d8618a14242fdf18b67fdc65
EddyL <kvl192@hotmail.com>
2016-05-17 00:25:34 -0700

    Implemented different types of request and put the scraping code into its own file

--- webapp.js
+++ webapp.js
@@ -1,5 +1,3 @@
-/** Server **/
-
 var express = require('express');
 var mongodb = require('mongodb').MongoClient;
 var bodyParser = require('body-parser');
@@ -7,8 +5,8 @@ var assert = require('assert'); // Unit Testing
 var passwordHash = require('password-hash');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
-var https = require("https");
 var schedule = require('node-schedule');
+var Scraping = require('./ScrapingAPI');
 
 var app = express();
 var app_port = 3000;
@@ -16,7 +14,7 @@ var db_url = 'mongodb://localhost/appdb';
 var db;
 var data; // JSON data fetch from AppTweak
 var req; // HTTPS request for AppTweak
-var curColl = "data"; // Current used collection
+var curColl = ""; // Current used collection
 var sched; // Schedule/frequency of fetching data
 
 app.use(bodyParser.json());
@@ -37,175 +35,6 @@ app.use(session({
 /**********************************************/
 /**** Scraping API ****************************/
 /**********************************************/
-
-
-
-//Information for the REST call
-var header = {"X-Apptweak-Key": "QS5NiFFrLERBRML_ptL208cJoWc"};
-var options = {
-	hostname: "api.apptweak.com",
-	port: 443,
-	path: "/ios/categories/6014/top.JSON",
-	"rejectUnauthorized": false,
-	method:"GET",
-	headers: header
-};
-
-/**
- * Requesting scraped data from AppTweak. If successful,
- * it'll drop the old collection of data if any, and then 
- * insert the new scraped data.
- */ 
-function requestToAppTweak() {
-	req = https.request(options, function(res) {
-		var responseBody =""; 
-		console.log("> Response from server started."); 
-		console.log(`> Server Status: ${res.statusCode}`); 
-		console.log("> Response Headers: %j", res.headers);
-		res.setEncoding("UTF-8"); 
-		//retrieve the data in chunks
-		res.on("data", function(chunk) {
-			responseBody += chunk;
-		});
-
-		res.on("end", function(){
-			//Once completed we parse the data in JSON format
-			data = JSON.parse(responseBody);
-			//Deleting old records to avoid overlaps
-			db.collection("data").drop();
-			//Inserting scraped data 
-			insertNewData();
-		});
-	});
-	
-	req.on("error", function(err) {
-		console.log(`problem with request: ${err.message}`);
-	});
-	req.end();
-}
-
-function insertNewData() {
-	var date = new Date();
-	var time = date.getTime();
-	// curColl = "data_" + time.toString();
-	
-	db.collection(curColl).insertMany(data.content, function(err,result) {
-		assert.equal(err, null);
-	});	
-	modifyDataAndUpdate();
-}
-
-function modifyDataAndUpdate() {
-	db.collection(curColl).find().each(function(err, doc) {
-		assert.equal(err, null);
-		if(doc != null) {
-			if(doc.price == "$0.00") {
-				var query = {_id: doc._id};
-				var update = {$set : {price: "Free"}};
-				db.collection(curColl).update(query, update);
-			}
-		
-			for(var i=0; i < doc.genres.length; i++) { 
-				// console.log("genre > ", doc.genres[i]);
-				var cat;
-				switch(doc.genres[i]) {
-					case 0:
-						cat = "All"; break;
-					case 6000:
-						cat = "Business"; break;
-					case 6001:
-						cat = "Weather"; break;
-					case 6002:
-						cat = "Utilities"; break;
-					case 6003:
-						cat = "Travel"; break;
-					case 6004:
-						cat = "Sports"; break;
-					case 6005:
-						cat = "Social Networking"; break;
-					case 6006:
-						cat = "Reference"; break;
-					case 6007:
-						cat = "Productivity"; break;
-					case 6008:
-						cat = "Photo & Video"; break;
-					case 6009:
-						cat = "News"; break;
-					case 6010: 
-						cat = "Navigation"; break;
-					case 6011:
-						cat = "Music"; break;
-					case 6012:
-						cat = "Lifestyle"; break;
-					case 6013:
-						cat = "Health & Fitness"; break;
-					case 6014:
-						cat = "Games"; break;
-					case 6015:
-						cat = "Finance"; break;
-					case 6016:
-						cat = "Entertainment"; break;
-					case 6017:
-						cat = "Education"; break;
-					case 6018:
-						cat = "Books"; break;
-					case 6020:
-						cat = "Medical"; break;
-					case 6021:
-						cat = "Newsstand"; break;
-					case 6022:
-						cat = "Catalogs"; break;
-					case 6023:
-						cat = "Food & Drink"; break;
-					case 7001:
-						cat = "Action"; break;
-					case 7002:
-						cat = "Adventure"; break;
-					case 7003:
-						cat = "Arcade"; break;
-					case 7004:
-						cat = "Board"; break;
-					case 7005:
-						cat = "Card"; break;
-					case 7006:
-						cat = "Casino"; break;
-					case 7007:
-						cat = "Dice"; break;
-					case 7008:
-						cat = "Educational"; break;
-					case 7009:
-						cat = "Family"; break;
-					case 7010:
-						cat = "Kids"; break;
-					case 7011:
-						cat = "Music"; break;
-					case 7012:
-						cat = "Puzzle"; break;
-					case 7013:
-						cat = "Racing"; break;
-					case 7014:
-						cat = "Role Playing"; break;
-					case 7015:
-						cat = "Simulation"; break;
-					case 7016:
-						cat = "Sports"; break;
-					case 7017:
-						cat = "Strategy"; break;
-					case 7018:
-						cat = "Trivia"; break;
-					case 7019:
-						cat = "Word"; break;
-					default:
-						cat = "";
-				}
-				var genresIndex = "genres."+i;
-				var query = {_id: doc._id};
-				var update = {$set : {[genresIndex]: cat}};
-				db.collection(curColl).update(query, update);
-			}
-		} 
-	});
-}
 
 //Route to display the information on the table -> filtering is work in progress
 app.get('/api/bugs', function(req,res){
@@ -229,6 +58,41 @@ app.post('/api/bugs/', function(req, res) {
 	bugData.push(newBug);
 	res.json(newBug);
 });
+
+function requestAPI() {
+	// For every minute: '*/1 * * * *'
+	// Scheduled a minute apart to prevent race conditions
+	/* sched = schedule.scheduleJob('1 0 * * *', function() {
+		requestToAppTweak("/ios/categories/6014/top.json");
+	});
+	sched = schedule.scheduleJob('2 0 * * *', function() {
+		requestToAppTweak("/ios/categories/6014/top.json?type=paid");
+	});
+	sched = schedule.scheduleJob('3 0 * * *', function() {
+		requestToAppTweak("/ios/categories/6014/top.json?type=grossing");
+	});
+	sched = schedule.scheduleJob('4 0 * * *', function() {
+		requestToAppTweak("/android/categories/game/top.json");
+	});
+	sched = schedule.scheduleJob('5 0 * * *', function() {
+		requestToAppTweak("/android/categories/game/top.json?type=paid");
+	});
+	sched = schedule.scheduleJob('6 0 * * *', function() {
+		requestToAppTweak("/android/categories/game/top.json?type=grossing");
+	}); */
+	
+	// Use these for testing only.git 
+	Scraping.requestToAppTweak("/ios/categories/6014/top.json", db, curColl);
+	// requestToAppTweak("/ios/categories/6014/top.json?type=paid");
+	// requestToAppTweak("/ios/categories/6014/top.json?type=grossing");
+	// requestToAppTweak("/android/categories/game/top.json");
+	// requestToAppTweak("/android/categories/game/top.json?type=paid");
+	// requestToAppTweak("/android/categories/game/top.json?type=grossing");
+	
+	curColl = Scraping.getColl();
+	console.log(">>>>>>This is curColl = ", curColl);
+}
+
 
 /**********************************************/
 /**** Login Authentication ********************/
@@ -332,17 +196,16 @@ app.post('/api/switchRole', function(req, res) {
 
 
 
+/**********************************************/
+/**** Server **********************************/
+/**********************************************/
 	
 // Connecting to the database
 mongodb.connect(db_url, function(err, dbConnection) {
 	assert.equal(null, err);
 	db = dbConnection;	
-	// sched = schedule.scheduleJob('*/1 * * * *', function() {
-		// requestToAppTweak();
-	// });
-	requestToAppTweak();
+	requestAPI();
 	
-	// Starting the server
 	var server = app.listen(app_port, function() {
 		console.log('> Application listening on port ' + app_port + '!');
 	});
